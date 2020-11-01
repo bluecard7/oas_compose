e8c8eabe63755936e010c2550a1cf3223face1dd
Paul Yang <palyang777@gmail.com>
2020-11-01 00:18:16 -0700

    added regex check on prepname

--- compose/prep_loader.py
+++ compose/prep_loader.py
@@ -1,9 +1,12 @@
+import re
 from compose import preps
 from compose.errormsg import nosuchprepmsg
 
 class PrepLoader:
     def getprep(self, prepname):
-        if not hasattr(preps, prepname):
+        prefix_match = re.match('^pre', prepname)
+        
+        if prefix_match is None or not hasattr(preps, prepname):
             raise NotImplementedError(nosuchprepmsg(prepname))
 
-        return getattr(preps, prepname) 
+        return getattr(preps, prepname)
