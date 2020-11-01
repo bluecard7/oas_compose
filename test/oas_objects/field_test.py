f74113cf813cd7ce559baf99842010f280476c27
Paul Yang <palyang777@gmail.com>
2020-11-01 00:14:12 -0700

    added field desc test

--- test/oas_objects/field_test.py
+++ test/oas_objects/field_test.py
@@ -1,5 +1,12 @@
 import pytest
 from compose.oas_objects.field import FieldDesc
 
-def test_d():
-    assert 1 == 1  
+states = [
+    (FieldDesc(), 'False', False), 
+    (FieldDesc(True), 'True', True)
+]
+
+@pytest.mark.parametrize('desc, repr_str, required', states)
+def test_field_desc_state(desc, repr_str, required):
+    assert repr(desc) == repr_str
+    assert desc.bare() == required
