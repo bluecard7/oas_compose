0064027b2471556bd064b655e51b8d15a1476801
Paul Yang <palyang777@gmail.com>
2020-11-01 00:16:47 -0700

    refactored parser_arguments

--- compose/entry.py
+++ compose/entry.py
@@ -1,5 +1,4 @@
 import argparse
-
 from compose.oas_writer import OASSpecWriter
 
 def compose(fragment_dirname, root_fragname, specname):
@@ -7,18 +6,14 @@ def compose(fragment_dirname, root_fragname, specname):
     writer.write_oas_spec()
 
 parser = argparse.ArgumentParser(description='Compose OAS spec from fragments')
-parser.add_argument(
-    '-d', dest='fragment_dirname', default='openapi', 
-    help='directory containing OAS fragments'
-)
-parser.add_argument(
-    '-r', dest='root_fragname', default='root.yaml', 
-    help='name of fragment to start writing spec from, should be located in fragment_dir'
-)
-parser.add_argument(
-    '-o', dest='specname', default='openapi.yaml', 
-    help='name of file to write spec to'
-)
+parser_arguments = [
+    ['-d', 'fragment_dirname', 'openapi', 'directory containing OAS fragments'],
+    ['-r', 'root_fragname', 'root.yaml', 'name of fragment to start writing spec from, should be in fragment_dir'],
+    ['-o', 'specname', 'openapi.yaml', 'name of file to write spec to'],
+]
+
+for option, dest_name, default_value, help_msg in parser_arguments:
+    parser.add_argument(option, dest=dest_name, default=default_value, help=help_msg)
 
 args = parser.parse_args()
 compose(args.fragment_dirname, args.root_fragname, args.specname)
