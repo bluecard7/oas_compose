7ce6b2849fa2ae9925d45b260aba8567b37ecbd8
Paul Yang <palyang777@gmail.com>
2020-11-01 00:17:20 -0700

    merged fieldname and prep name handling

--- compose/oas_writer.py
+++ compose/oas_writer.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 
 from compose.oas_objects.openapi import OpenAPI
 from compose.prep_loader import PrepLoader
@@ -26,26 +27,34 @@ class OASSpecWriter:
 
         del self.spec
     
-    def walk_oas_tree(self, obj_type, obj_dict):
-        for fieldname in obj_type.field_order:
-            fieldDesc = getattr(obj_type, fieldname)
+    def walk_oas_tree(self, objtype, objdict):
+        for fieldname in objtype.field_order:
+            fieldDesc = getattr(objtype, fieldname)
             required = fieldDesc.bare()
-            prepname = f'pre{fieldname}'
 
-            if fieldname in obj_dict:
-                data = obj_dict[fieldname]
-                self.write_to_spec({ fieldname: data })
-                del obj_dict[fieldname]
-
-            elif prepname in obj_dict:
-                prep = self.prep_loader.getprep(prepname)
-                data = obj_dict[prepname]
-                prepped_data = prep(self.fragment_dirname, data)
-                self.write_to_spec({ fieldname: prepped_data })
-                del obj_dict[prepname]
+            name = self.field_satisfying_name(fieldname, objdict)
+            if name is not None:
+                data = self.take_data(objdict, name)
+                yaml.dump({ fieldname: data }, self.spec)
 
             elif required:
                 raise RuntimeError(fieldrequiredmsg(fieldname))
 
-    def write_to_spec(self, field):
-        yaml.dump(field, self.spec)
+    def field_satisfying_name(self, fieldname, objdict):
+        if fieldname in objdict:
+            return fieldname
+
+        elif f'pre{fieldname}' in objdict:
+            return f'pre{fieldname}'
+
+        return None
+
+    def take_data(self, objdict, name):
+        data = objdict[name]
+
+        if re.match('^pre', name) is not None:
+            prep = self.prep_loader.getprep(name)
+            data = prep(self.fragment_dirname, data)
+        
+        del objdict[name]
+        return data
